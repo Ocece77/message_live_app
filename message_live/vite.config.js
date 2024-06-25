e28380b9b37e89306febb109cf69fd112eae5d3c
Ocece77 <oceanekasindupro@gmail.com>
2024-06-25 10:48:04 +0200

    change the link

--- message_live/vite.config.js
+++ message_live/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   proxy : {
     '/api':{
-      target :'http://localhost:3000',
+      target :'https://message-live-app.onrender.com',
       changeOrigin: true,
       secure: false,      
       ws: true,
