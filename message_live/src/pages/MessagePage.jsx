7ea6c486d21d50796df115e6b494902c1ee23189
Ocece77 <oceanekasindupro@gmail.com>
2024-06-25 11:12:27 +0200

    config

--- message_live/src/pages/MessagePage.jsx
+++ message_live/src/pages/MessagePage.jsx
@@ -29,7 +29,7 @@ const MessagePage =()=>{
   const handleSubmit= async (e)=>{
       e.preventDefault();
       try{
-        const res = await fetch('http://localhost:3000/api/message/post', {
+        const res = await fetch('https://message-live-app.onrender.com/api/message/post', {
           method : 'POST',
           headers : {'Content-Type'  : 'application/json' },
           body : JSON.stringify(formData)
@@ -47,7 +47,7 @@ const MessagePage =()=>{
   useEffect(()=>{
 
       try {
-        fetch('http://localhost:3000/api/message/get')
+        fetch('https://message-live-app.onrender.com/api/message/get')
         .then(response => {
           if (!response.ok){
           console.error('Error in the retrievement of the message')
--- message_live/vite.config.js
+++ message_live/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   proxy : {
     '/api':{
-      target :'http://localhost:3000',
+      target :'https://message-live-app.onrender.com',
       changeOrigin: true,
-      secure: false,      
+      secure: true,      
       ws: true,
     }
   },
