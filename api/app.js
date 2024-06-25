8b425cbf5a54731f2cb3e09f7929090474fab7f8
Ocece77 <oceanekasindupro@gmail.com>
2024-06-25 11:21:28 +0200

    helmet config

--- api/app.js
+++ api/app.js
@@ -1,32 +1,46 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import 'dotenv/config'
+import 'dotenv/config';
 import messageRouter from './routes/message.route.js';
 import cookieParser from 'cookie-parser';
-
+import helmet from 'helmet';
 
 const app = express();
-const PORT = (process.env.PORT || 3000)
+const PORT = process.env.PORT || 3000;
 app.use(cookieParser());
 
+// Configuration des options CORS
 const corsOptions = {
-  origin: "https://message-live-app-client.onrender.com", // frontend URI (ReactJS)
-}
+  origin: "https://message-live-app-client.onrender.com", // URI du frontend (ReactJS)
+};
 
-app.use(express.json())
-app.use(cors(corsOptions))
+app.use(express.json());
+app.use(cors(corsOptions));
 
+// Utilisation de Helmet pour la sécurité
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      connectSrc: ["'self'", "https://message-live-app.onrender.com"],
+      imgSrc: ["'self'"],
+      scriptSrc: ["'self'"]
+    },
+  })
+);
 
+// Connexion à la base de données MongoDB
 mongoose.connect(process.env.MONGO)
-.then(()=>console.log("The database as been connected"))
-.catch((err)=> console.log(err))
+  .then(() => console.log("The database has been connected"))
+  .catch((err) => console.log(err));
 
+// Routes de l'application
+app.use("/api/message", messageRouter);
 
-app.use( "/api/message",messageRouter)
-
-
-app.listen(PORT , ()=>{
-  console.log('This app is running on port' ,PORT)
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log('This app is running on port', PORT);
 });
-
