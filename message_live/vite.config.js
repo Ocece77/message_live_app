9ad3d4bb4c00ce94a43431cc442c84960960e585
Ocece77 <oceanekasindupro@gmail.com>
2024-06-28 00:02:19 +0200

    changing the vite config

--- message_live/vite.config.js
+++ message_live/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  proxy : {
-    '/api':{
-      target :'http://http://localhost:3000',
-      secure: true,      
-    }
-  },
   plugins: [react()],
   base: "https://message-live-app.vercel.app",
-
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: true,
+        changeOrigin: true,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx'],
+  },
+});
