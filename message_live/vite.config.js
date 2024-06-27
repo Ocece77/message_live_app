e432224453a23109c1da55bffed32c4a695084d4
Ocece77 <oceanekasindupro@gmail.com>
2024-06-28 01:51:33 +0200

    Deploy React app to GitHub Pages

--- message_live/vite.config.js
+++ message_live/vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import babel from '@rollup/plugin-babel';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 export default defineConfig({
-  plugins: [react(),
-    babel({
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-    }),
-  ],
-  base: "/",
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
@@ -26,13 +13,4 @@ export default defineConfig({
       },
     },
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.js', '.jsx'],
-  },
-  build: {
-    outDir: 'dist'
-  }
 });
