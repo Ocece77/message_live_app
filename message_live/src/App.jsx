6ffce492bc84f2eb6a847b1ad92b5e8d8dbb175f
Ocece77 <oceanekasindupro@gmail.com>
2024-06-28 02:21:03 +0200

    -b master

--- message_live/src/App.jsx
+++ message_live/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes , Route } from "react-router-dom"
+import { HashRouter , Route } from "react-router-dom"
 import MessagePage from "./pages/MessagePage"
 import Navbar from "./components/Navbar"
 import Error from "./components/Error"
@@ -7,10 +7,10 @@ function App() {
   return (
     <>
     <Navbar/>
-      <Routes>
+      <HashRouter>
         <Route path="/message_live" element={<MessagePage/>}/>
         <Route path="*" element={<Error/>}/> 
-      </Routes>
+      </HashRouter>
     </>
   )
 }
