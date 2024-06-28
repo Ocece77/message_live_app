01805b7a07fc5aed646be7becec95470e1890642
Ocece77 <oceanekasindupro@gmail.com>
2024-06-28 02:12:10 +0200

    commit

--- message_live/vite.config.js
+++ message_live/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx'] 
+  },
+
+   build: {
+    outDir: 'dist', // Assurez-vous que c'est le bon chemin de sortie
+  },
   server: {
     proxy: {
       '/api': {
